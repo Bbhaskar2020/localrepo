4c260ec154f8fdb0b2e2d84a31660728d596a811
Bbhaskar2020 <bbhaskarjyotiphukan@yahoo.com>
2025-04-02 00:25:19 +0530

    new changes on code

--- Bubblesort.py
+++ Bubblesort.py
@@ -2,7 +2,7 @@
 arr1 = [7, 5, 1, 9, 2, 3, 4, 6]
 n = len(arr1)
 for i in range(n):
-        swapped = False
+    swapped = False
     for j in range(0, n-0-1):
         if arr1[j]>arr1[j+1]:
             arr1[j], arr1[j+1]=arr1[j+1], arr1[j]
@@ -12,15 +12,14 @@ for i in range(n):
 print("Sorted array..", arr1)
 
 
-
-arr2 = [8, 4, 6, 9, 1, 2]
-n = len(arr2)
-for i in range(n):
+list = [20, 100, 2, 50]
+count = len(list)
+for i in range(count):
     swapped = False
-    for j in range(0, n-0-1):
-        if arr2[j]>arr2[j+1]:
-            arr2[j], arr2[j+1] = arr2[j+1], arr2[j]
+    for j in range(0, count-0-1):
+        if list[j]>list[j+1]:
+            list[j], list[j+1]= list[j+1], list[j]
             swapped = True
     if not swapped:
         break
-print("sorted array..", arr2)
+print("Number swapped..", list)
