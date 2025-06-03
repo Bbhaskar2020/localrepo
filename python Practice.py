17c60a496e61b864f5b56bfb19cf2e6935bc0287
Bbhaskar2020 <bbhaskarjyotiphukan@yahoo.com>
2025-06-03 13:33:00 +0530

    newly added bubble sort function

--- python Practice.py
+++ python Practice.py
@@ -1,6 +1,13 @@
 
-a = 10
-b = 20
-def addnumber(a, b):
-    return a+b
-print(a+b)
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-0-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+arr = [1, 5,3,9, 8, 70, 4, 2]
+sorted_array = bubble_sort(arr)
+print("These are sorted array..", sorted_array)
+
